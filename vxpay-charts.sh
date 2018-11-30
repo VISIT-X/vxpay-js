29748a3136ab3f26bce1226b9552d87a280a2fd8
Igor Timoshenkov <igor.timoshenkov@gmail.com>
2018-11-30 12:53:31 +0300

    Feature mermaid (#21)
    * Add mermaid-js (cli) for flow cahrts
    * Add a flow chart
    * [Add sanitize](https://stackoverflow.com/a/16462143/1847769)
    * Mermaid config, more notes on chart
    * Include README.md in npm package
    * Include README.md in npm package; increment (patch)
    * Add several more diagrams

---
+++ vxpay-charts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./node_modules/.bin/mmdc -i docs/flows/IsLoggedInAction.flow -o docs/charts/IsLoggedInAction.svg -b #FFF
+./node_modules/.bin/mmdc -i docs/flows/OpenRegisterCommand.flow -o docs/charts/OpenRegisterCommand.svg -b #FFF
+./node_modules/.bin/mmdc -i docs/flows/OpenLoginCommand.flow -o docs/charts/OpenLoginCommand.svg -b #FFF
